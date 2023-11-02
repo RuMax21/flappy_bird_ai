4cbdc0ecfe4bcb43a25642faa06138d4bf5ea6a6
RuMax21 <cherednikov.maxim@mail.ru>
2023-11-02 05:19:23 +0300

    adding neat-python

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import neat
 import sys, time
+import os
 from random import randint
 
 pygame.init()
@@ -17,26 +18,29 @@ class Game:
         self.background = pygame.transform.scale(pygame.image.load('images/background.png').convert_alpha(), (600, 900))
 
         self.ground = Ground()
-        self.bird = Bird()
+        self.birds = []
         self.tubes = []
 
         self.tube_generate_counter = 71
         self.score = 0
 
-        self.passed = False
+        self.ge = []
+
         self.score_monitoring = False
-        self.is_game_started = False
+        self.is_game_started = True
 
         self.clock = pygame.time.Clock()
-        self.game_loop()
+        # self.game_loop()
 
+    #отрисовка всех объектов на экране
     def drawing_objects(self, delta) -> None:
         self.screen.blit(self.background, (0, 0))
-        self.screen.blit(self.bird.image_frame, self.bird.rect)
+        for bird in self.birds:
+            self.screen.blit(bird.image_frame, bird.rect)
         
         for tube in self.tubes:
             self.screen.blit(tube.tube_top, tube.rect_top)
-            self.screen.blit(tube.tube_bottom, tube.rect_down)
+            self.screen.blit(tube.tube_down, tube.rect_down)
         
         self.screen.blit(self.ground.ground_image, (self.ground.start_x, self.ground.y))
         self.screen.blit(self.ground.ground_image, (self.ground.end_x, self.ground.y))
@@ -44,12 +48,13 @@ class Game:
         score_label = pygame.font.SysFont("comicsans", 50).render("Score: " + str(self.score),1,(255,255,255))
         self.screen.blit(score_label, (self.screen_width - score_label.get_width() - 15, 10))
 
-    def updating_objects(self, delta: float) -> None:
-        if self.is_game_started:
-            self.ground.move(delta)
-            self.bird.update(delta)
-            self.working_tubes(delta)
-            self.counter_score()
+    # обновление оъектов
+    # def updating_objects(self, delta: float) -> None:
+    #     if self.is_game_started:
+    #         self.ground.move(delta)
+    #         self.bird.update(delta)
+    #         self.working_tubes(delta)
+    #         self.counter_score()
 
     def working_tubes(self, delta: float) -> None:
         if self.tube_generate_counter > 70:
@@ -64,61 +69,105 @@ class Game:
             if self.tubes[0].rect_top.right < 0:
                 self.tubes.pop(0)
 
-    def counter_score(self) -> None:
+    def counter_score(self, bird) -> None:
         if len(self.tubes) > 0:
-            if self.bird.rect.left > self.tubes[0].rect_down.left and self.bird.rect.right < self.tubes[0].rect_down.right and not self.score_monitoring:
+            if bird.rect.left > self.tubes[0].rect_down.left and bird.rect.right < self.tubes[0].rect_down.right and not self.score_monitoring:
                 self.score_monitoring = True
-            if self.bird.rect.left > self.tubes[0].rect_down.right and self.score_monitoring:
+            if bird.rect.left > self.tubes[0].rect_down.right and self.score_monitoring:
                 self.score_monitoring = False
                 self.score += 1
-            
-    def game_loop(self) -> None:
-        last_time = time.time()
-        while True:
-            new_time = time.time()
-            delta = new_time - last_time
-            last_time = new_time
+                for genome in self.ge:
+                    genome.fitness += 10
 
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_SPACE:
-                        self.bird.horizontal_offset(delta)
-                        self.is_game_started = True
-                    if event.key == pygame.K_RETURN:
-                        self.restart_game()
-            
-            self.drawing_objects(delta)
-
-            self.updating_objects(delta)
-
-            self.check_flying()
-
-            pygame.display.update()
-            self.clock.tick(60)
-
-    def check_flying(self) -> None:
+    def collide_checking(self, bird) -> None:
         if len(self.tubes):
-            if (self.bird.rect.bottom > 730) or self.bird.rect.colliderect(self.tubes[0].rect_top) or self.bird.rect.colliderect(self.tubes[0].rect_down):
-                self.bird.is_flying = False
-                self.is_game_started = False
-                self.drawing_text_to_restart()
+            if (bird.rect.bottom > 730) or (bird.rect.top < 0) or bird.rect.colliderect(self.tubes[0].rect_top) or bird.rect.colliderect(self.tubes[0].rect_down):
+                return True
 
     def restart_game(self) -> None:
         self.score = 0
         self.tubes.clear()
-        self.is_game_started = True
-        self.bird.is_flying = True
-        self.bird.rect = self.bird.image_frame.get_rect(center = (100, 100))
 
-    def drawing_text_to_restart(self) -> None:
-        restart_label = pygame.font.SysFont("comicsans", 50).render('press "enter" to restart',1,(255,255,255))
-        self.screen.blit(restart_label, (150, 350))
+    def neat_run(self, config_path):
+        config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                         neat.DefaultSpeciesSet, neat.DefaultStagnation,
+                         config_path)
+        population = neat.Population(config)
+
+        population.add_reporter(neat.StdOutReporter(True))
+        statistics = neat.StatisticsReporter()
+        population.add_reporter(statistics)
+
+        winner = population.run(self.game_loop, 50)
+
+    def game_loop(self, genomes, config) -> None:
+
+        nets = []
+
+        for genome_id, genome in genomes:
+            genome.fitness = 0
+            net = neat.nn.FeedForwardNetwork.create(genome, config)
+            nets.append(net)
+            self.birds.append(Bird())
+            self.ge.append(genome)
+
+
+        run = True
+        last_time = time.time()
+        while run:
+            new_time = time.time()
+            delta = new_time - last_time
+            last_time = new_time
+
+            if len(self.birds) == 0:
+                run = False
+                self.restart_game()
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+                    pygame.quit()
+                    sys.exit()
+
+            pipe_ind = 0
+            if len(self.birds) > 0:
+                if len(self.tubes) > 1 and self.birds[0].rect.x > self.tubes[0].rect_top.x + self.tubes[0].tube_top.get_width():
+                    pipe_ind = 1
+
+            for x, bird in enumerate(self.birds):
+                self.ge[x].fitness += 0.1
+                # bird.move()
+
+                if len(self.tubes) > 0:
+                    output = nets[self.birds.index(bird)].activate((bird.rect.y, abs(bird.rect.y - self.tubes[pipe_ind].rect_top.y), abs(bird.rect.y - self.tubes[pipe_ind].rect_down.y )))
+
+                    if output[0] > 0.5:
+                        bird.moving_bird(delta)
+
+            self.ground.move(delta)
+
+            self.working_tubes(delta)
+
+            for tube in self.tubes:
+                tube.update(delta)
+            # check for collision
+            for bird in self.birds:
+                bird.update(delta)
+                self.counter_score(bird)
+                if self.collide_checking(bird):
+                    self.ge[self.birds.index(bird)].fitness -= 2
+                    nets.pop(self.birds.index(bird))
+                    self.ge.pop(self.birds.index(bird))
+                    self.birds.pop(self.birds.index(bird))
+
+            pygame.display.update()
+            self.clock.tick(60)
+
+            self.drawing_objects(delta)
+
 
 class Ground:
-    SPEED: int = 250
+    SPEED: int = 200
     SPAWN_Y: int = 730
     def __init__(self) -> None:
         self.ground_speed = self.SPEED
@@ -129,7 +178,7 @@ class Ground:
         self.end_x = self.ground_width
         self.y = self.SPAWN_Y
 
-    def move(self, delta) -> None:
+    def move(self, delta: float) -> None:
         self.start_x -= self.ground_speed * delta
         self.end_x -= self.ground_speed * delta
         
@@ -140,18 +189,20 @@ class Ground:
             self.end_x = self.start_x + self.ground_width
 
 class Bird:
+    FLAP: int = 300
+    GRAVITY: int = 10
     def __init__(self) -> None:
         self.images_of_birds = [pygame.transform.scale2x(pygame.image.load('images/bird1.png').convert_alpha()),
                                 pygame.transform.scale2x(pygame.image.load('images/bird2.png').convert_alpha()),
                                 pygame.transform.scale2x(pygame.image.load('images/bird3.png').convert_alpha())]
         self.index_frame = 0
+        self.animation_count = 0
         self.image_frame = self.images_of_birds[self.index_frame]
         self.rect = self.image_frame.get_rect(center = (100, 100))
-        self.animation_count = 0
 
-        self.gravity = 10
+        self.gravity = self.GRAVITY
+        self.flap = self.FLAP
         self.speed_y = 0
-        self.speed_x = 300
 
         self.is_flying = True
     
@@ -161,8 +212,8 @@ class Bird:
             self.rect.y += self.speed_y
             self.animation()
         
-    def horizontal_offset(self, delta: float) -> None:
-        self.speed_y = -self.speed_x * delta
+    def moving_bird(self, delta: float) -> None:
+        self.speed_y = -self.flap * delta
 
     def animation(self) -> None:
         if self.animation_count < 5:
@@ -178,27 +229,32 @@ class Bird:
         self.animation_count += 1
 
 class Tube:
-    MOVE_SPEED: int = 250
-    DISTANCE: int = 150
+    MOVE_SPEED: int = 200
+    DISTANCE: int = 200
     SPAWN_TUBE_X: int = 700
+
     def __init__(self) -> None:
         self.image = pygame.transform.scale2x(pygame.image.load('images/tube.png').convert_alpha())
-        self.tube_bottom = pygame.transform.flip(self.image, False, True)
+        self.tube_down = pygame.transform.flip(self.image, False, True)
         self.tube_top = self.image
-        self.rect_top = self.tube_top.get_rect()
-        self.rect_down = self.tube_bottom.get_rect()
+
+        self.move_speed = self.MOVE_SPEED
         self.distance = self.DISTANCE
+
+        self.rect_top = self.tube_top.get_rect()
+        self.rect_down = self.tube_down.get_rect()
        
         self.rect_top.y = randint(250, 520)
         self.rect_top.x = self.SPAWN_TUBE_X
         self.rect_down.y = self.rect_top.y - self.distance - self.rect_top.height
         self.rect_down.x = self.SPAWN_TUBE_X
         
-        self.move_speed = self.MOVE_SPEED
-
     def update(self, delta: float) -> None:
         self.rect_top.x -= self.move_speed * delta
         self.rect_down.x -= self.move_speed * delta
 
-
-game = Game()
+if __name__ == "__main__":
+    game = Game()
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, 'config')
+    game.neat_run(config_path)
